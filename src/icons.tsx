f7e2ed73146fd6773bd4e07f30c44a458aaf30eb
Raye <astingraye@proton.me>
2025-04-17 02:28:12 +0100

    Make icons slightly smaller

--- src/icons.tsx
+++ src/icons.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export function LinkIcon({ icon, url }) {
 	return (
-        <Icon icon={icon} height={44} width={44} className='h-fit cursor-pointer'/>
+        <Icon icon={icon} height={34} width={34} className='h-fit cursor-pointer'/>
     )
 }
