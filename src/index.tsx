b02f1b938d9e2b45a6f68354843212d7070509e9
Raye <astingraye@proton.me>
2025-07-01 00:27:49 +0100

    remove projects (it kinda sucked)

--- src/index.tsx
+++ src/index.tsx
@@ -3,102 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { snake } from './snake';
 import { LinkIcon } from './icons';
-import { Icon } from '@iconify-icon/react/dist/iconify.js';
-import { Project } from './components/Project';
-
-function Screen({ children }) {
-	return (
-		<div className="min-h-screen h-screen w-screen flex items-center justify-center">
-			{children}
-		</div>
-	);
-}
 
 function App() {
-	const cvRef = useRef<HTMLCanvasElement>(null);
-	const heightRef = useRef<HTMLDivElement>(null);
-	const [height, setHeight] = useState(0);
+	const cvRef = useRef<HTMLCanvasElement>();
 
+	// cursed
 	useEffect(() => {
-		if (heightRef.current) {
-			setHeight(heightRef.current.clientHeight);
-		}
+		snake(cvRef.current);
 	}, []);
 
-	useEffect(() => {
-		if (cvRef.current && height > 0) {
-			cvRef.current.height = height;
-			cvRef.current.width = window.innerWidth;
-			snake(cvRef.current);
-		}
-	}, [cvRef.current, height]);
-
 	return (
-		<div
-			ref={heightRef}
-			className="text-ctp-text bg-ctp-base text-4xl flex flex-col items-center p-10 h-fit min-h-screen w-screen relative"
-		>
-			{height > 0 && (
-				<canvas
-					ref={cvRef}
-					className="w-screen absolute z-10 pointer-events-none top-0 left-0"
-					style={{
-						imageRendering: 'pixelated',
-						height: `${height}px`,
-					}}
-				/>
-			)}
-			<Screen>
-				<div className="lg:scale-100 scale-150 w-full h-full flex flex-col gap-2 items-center justify-center rounded-lg p-10">
-					<h1>I'm s4mi, I make things (badly)</h1>
-					<div className="flex flex-row gap-4 h-fit">
-						<LinkIcon
-							icon={'pixelarticons:github'}
-							url={'https://github.com/s4midev'}
-						/>
-						<LinkIcon
-							icon={'pixelarticons:chat'}
-							url={
-								'https://discordapp.com/users/1298435571395330108'
-							}
-						/>
-					</div>
+		<div className="text-ctp-text bg-ctp-base text-4xl flex items-center justify-center h-screen w-screen">
+			<div className="lg:scale-100 scale-150 w-fit h-fit m-24 flex flex-col gap-6 items-center justify-center rounded-lg p-10">
+				<h1>I'm s4mi, I make things (badly)</h1>
+				<div className='flex flex-row gap-4 h-fit'>
+					<LinkIcon icon={"pixelarticons:github"} url={"https://github.com/s4midev"}/>
+					<LinkIcon icon={"pixelarticons:chat"} url={"https://discordapp.com/users/1298435571395330108"}/>
 				</div>
-			</Screen>
-			<Screen>
-				<div className="lg:scale-100 scale-150 w-full h-full flex flex-col gap-2 items-center justify-center rounded-lg p-10">
-					<h1>Projects I use and recommend</h1>
-					<div className="flex justify-center lg:flex-row md:flex-col sm:flex-col gap-2 flex-wrap">
-						{[
-							{
-								name: 'Jellyfin',
-								asset: 'jellyfin.png',
-								blurb: 'The Free Software Media System',
-								url: 'https://jellyfin.org',
-							},
-							{
-								name: 'iPlayarr',
-								asset: 'iplayarr.png',
-								blurb: 'An *arr companion app for get_iplayer',
-								url: 'https://github.com/Nikorag/iplayarr',
-							},
-							{
-								name: 'Immich',
-								asset: 'immich.png',
-								blurb: 'Self-hosted media management solution',
-								url: 'https://immich.app',
-							}
-						].map((e) => (
-							<Project {...e} />
-						))}
-					</div>
-					<h1 className="text-2xl text-ctp-subtext0">
-						<i>
-							I'm not endorsed by any of these, they're just cool!
-						</i>
-					</h1>
-				</div>
-			</Screen>
+			</div>
+			<canvas
+				ref={cvRef}
+				className="w-screen h-screen fixed z-10 pointer-events-none"
+				style={{
+					imageRendering: 'pixelated',
+				}}
+			/>
 		</div>
 	);
 }
