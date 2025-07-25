3897c108bafd5f3c0f3f9494fbc1c043d648ce64
Raye <astingraye@proton.me>
2025-07-25 11:19:17 +0100

    add petition

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { snake } from './snake';
 import { LinkIcon } from './icons';
+import { Icon } from '@iconify-icon/react/dist/iconify.js';
 
 function App() {
 	const cvRef = useRef<HTMLCanvasElement>();
@@ -16,9 +17,15 @@ function App() {
 		<div className="text-ctp-text bg-ctp-base text-4xl flex items-center justify-center h-screen w-screen">
 			<div className="lg:scale-100 scale-150 w-fit h-fit m-24 flex flex-col gap-6 items-center justify-center rounded-lg p-10">
 				<h1>I'm s4mi, I make things</h1>
-				<div className='flex flex-row gap-4 h-fit'>
-					<LinkIcon icon={"pixelarticons:github"} url={"https://github.com/s4midev"}/>
-					<LinkIcon icon={"pixelarticons:chat"} url={"https://discordapp.com/users/1298435571395330108"}/>
+				<div className="flex flex-row gap-4 h-fit">
+					<LinkIcon
+						icon={'pixelarticons:github'}
+						url={'https://github.com/s4midev'}
+					/>
+					<LinkIcon
+						icon={'pixelarticons:chat'}
+						url={'https://discordapp.com/users/1298435571395330108'}
+					/>
 				</div>
 			</div>
 			<canvas
@@ -28,6 +35,29 @@ function App() {
 					imageRendering: 'pixelated',
 				}}
 			/>
+			<div
+				className="absolute bottom-2 w-full flex flex-row items-center justify-center text-lg text-ctp-yellow cursor-pointer gap-4"
+				onClick={() => {
+					window.open(
+						'https://petition.parliament.uk/petitions/722903',
+						'_blank'
+					);
+				}}
+			>
+				<Icon
+					icon={'pixelarticons:warning-box'}
+					height={32}
+					width={32}
+				/>
+				<h1>
+					If you are in the UK, please consider signing this petition!
+				</h1>
+				<Icon
+					icon={'pixelarticons:warning-box'}
+					height={32}
+					width={32}
+				/>
+			</div>
 		</div>
 	);
 }
